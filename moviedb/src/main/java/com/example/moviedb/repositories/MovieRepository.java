65e5374c124d96631d24c16dcb8993ac8afed90a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-12-31 09:38:45 +0100

    extended the movierepository interface with the mongorespoitory

--- moviedb/src/main/java/com/example/moviedb/repositories/MovieRepository.java
+++ moviedb/src/main/java/com/example/moviedb/repositories/MovieRepository.java
@@ -1,4 +1,13 @@
 package com.example.moviedb.repositories;
 
-public interface MovieRepository {
+import com.example.moviedb.entities.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    Optional<Movie> findMovieByImdbId(String imdbId);
 }
