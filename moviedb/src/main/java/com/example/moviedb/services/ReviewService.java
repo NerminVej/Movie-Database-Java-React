5114fcd708622a6ef0241d0e83ce13564c4e1ece
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-01 18:44:00 +0100

    started to create the createReview method

--- moviedb/src/main/java/com/example/moviedb/services/ReviewService.java
+++ moviedb/src/main/java/com/example/moviedb/services/ReviewService.java
@@ -1,4 +1,28 @@
 package com.example.moviedb.services;
 
+import com.example.moviedb.entities.Movie;
+import com.example.moviedb.entities.Review;
+import com.example.moviedb.repositories.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
 public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        
+
+        return review;
+    }
 }
