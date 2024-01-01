540925ea902fff3121db8e69a1888e572e325c33
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-01 18:44:13 +0100

    finished creating the createReview method

--- moviedb/src/main/java/com/example/moviedb/services/ReviewService.java
+++ moviedb/src/main/java/com/example/moviedb/services/ReviewService.java
@@ -21,7 +21,12 @@ public class ReviewService {
     private MongoTemplate mongoTemplate;
 
     public Review createReview(String reviewBody, String imdbId) {
-        
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviews").value(review))
+                .first();
 
         return review;
     }
