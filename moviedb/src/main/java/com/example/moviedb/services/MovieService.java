ebd84c6c0195bc0f2e8411c8ef8c2ff4d6ca315e
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-02 18:41:13 +0100

    created the findAllMovies method

--- moviedb/src/main/java/com/example/moviedb/services/MovieService.java
+++ moviedb/src/main/java/com/example/moviedb/services/MovieService.java
@@ -1,4 +1,21 @@
 package com.example.moviedb.services;
 
+import com.example.moviedb.entities.Movie;
+import com.example.moviedb.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MovieService {
+
+    @Autowired
+    private MovieRepository repository;
+
+    public List<Movie> findAllMovies() {
+        return repository.findAll();
+    }
+
 }
