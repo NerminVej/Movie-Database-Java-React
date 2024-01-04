5241497e43a77dea4286077ed262d69558a0f565
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-04 18:15:13 +0100

    added the restcontroller annotation aswell as the requestmapping annotation

--- moviedb/src/main/java/com/example/moviedb/controllers/MovieController.java
+++ moviedb/src/main/java/com/example/moviedb/controllers/MovieController.java
@@ -1,4 +1,10 @@
 package com.example.moviedb.controllers;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
 }
