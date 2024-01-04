aa6f5c28c0d26acf3956f0d1a0aed94efc85325b
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-04 18:15:41 +0100

    created the getMovies get method

--- moviedb/src/main/java/com/example/moviedb/controllers/MovieController.java
+++ moviedb/src/main/java/com/example/moviedb/controllers/MovieController.java
@@ -1,10 +1,27 @@
 package com.example.moviedb.controllers;
 
 
+import com.example.moviedb.entities.Movie;
+import com.example.moviedb.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    @Autowired
+    private MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(service.findAllMovies(), HttpStatus.OK);
+    }
+
 }
