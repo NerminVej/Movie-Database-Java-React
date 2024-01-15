22b1c000b483165a2635b85e424d0053ae1af907
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-15 20:12:48 +0100

    created the Home component

--- movie-frontend/src/App.tsx
+++ movie-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Trailer } from "./components/Trailer";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Hero />
       <Trailer />
+      <Home />
     </Router>
   );
 }
