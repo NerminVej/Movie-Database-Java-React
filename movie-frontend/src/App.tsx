b26a51c1b76cd49e8d76f7bd94322387316b8d58
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-15 20:16:04 +0100

    testing out the react router route creation

--- movie-frontend/src/App.tsx
+++ movie-frontend/src/App.tsx
@@ -1,20 +1,18 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import { Trailer } from "./components/Trailer";
 import Home from "./components/Home";
 import { useState } from "react";
 
 function App() {
-
-  
   const [movies, setMovies] = useState();
   const [movie, setMovie] = useState();
   const [reviews, setReviews] = useState([]);
 
-  
   return (
     <Router>
+      <Route path="/" element={<Header />}></Route>
       <Header />
       <Hero />
       <Trailer />
