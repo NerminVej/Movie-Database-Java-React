74e6ae428743a4bda9197a010b77be7331a245f2
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-18 17:26:39 +0100

    created the getmoviedata and getmovies methods for now in the app file

--- movie-frontend/src/App.tsx
+++ movie-frontend/src/App.tsx
@@ -3,12 +3,53 @@ import Hero from "./components/Hero";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import { Trailer } from "./components/Trailer";
 import Home from "./components/Home";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState();
   const [movie, setMovie] = useState();
   const [reviews, setReviews] = useState([]);
+
+  const getMovies = async () =>{
+    
+    try
+    {
+
+      const response = await api.get("/api/v1/movies");
+
+      setMovies(response.data);
+
+    } 
+    catch(err)
+    {
+      console.log(err);
+    }
+  }
+
+  const getMovieData = async (movieId) => {
+     
+    try 
+    {
+        const response = await api.get(`/api/v1/movies/${movieId}`);
+
+        const singleMovie = response.data;
+
+        setMovie(singleMovie);
+
+        setReviews(singleMovie.reviews);
+        
+
+    } 
+    catch (error) 
+    {
+      console.error(error);
+    }
+
+  }
+
+  useEffect(() => {
+    getMovies();
+  },[])
 
   return (
     <Router>
