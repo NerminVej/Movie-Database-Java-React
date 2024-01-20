f891d6c30f3498325ce07eba25239ac64f21c13a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-20 09:25:10 +0100

    finished creating the ReviewForm

--- movie-frontend/src/components/ReviewForm.tsx
+++ movie-frontend/src/components/ReviewForm.tsx
@@ -1,8 +1,30 @@
-
-const ReviewForm = () => {
+const ReviewForm = ({ handleSubmit, revText, labelText, defaultValue }) => {
   return (
-    <div>ReviewForm</div>
-  )
-}
+    <form>
+      <div className="mb-3">
+        <label
+          htmlFor="review"
+          className="block text-sm font-medium text-gray-700"
+        >
+          {labelText}
+        </label>
+        <textarea
+          ref={revText}
+          id="review"
+          rows={3}
+          className="mt-1 p-2 block w-full border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+          defaultValue={defaultValue}
+        ></textarea>
+      </div>
+      <button
+        type="button"
+        onClick={handleSubmit}
+        className="mt-2 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+      >
+        Submit
+      </button>
+    </form>
+  );
+};
 
-export default ReviewForm
+export default ReviewForm;
