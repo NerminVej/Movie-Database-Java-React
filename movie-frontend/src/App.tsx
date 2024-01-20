f72726c469287b2e39eb0a7d8cace4ebd6ef0a13
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-20 09:31:31 +0100

    created all the routes for the application

--- movie-frontend/src/App.tsx
+++ movie-frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Trailer } from "./components/Trailer";
 import Home from "./components/Home";
 import { useEffect, useState } from "react";
 import api from './api/axiosConfig';
+import Layout from "./components/Layout";
+import NotFound from "./components/NotFound";
+import Reviews from "./components/Reviews";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -54,10 +57,16 @@ function App() {
 
   return (
     <Router>
-      <Route path="/" element={<Header />}></Route>
-      <Header />
-      <Trailer />
-      <Home />
+      <Header/>
+      <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route>
+          </Route>
+      </Routes>
+
     </Router>
   );
 }
