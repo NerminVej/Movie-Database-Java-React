d22d61447bca00798d1757ca1bc13d39585c7d7a
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2024-01-20 09:26:05 +0100

    created the Trailer component

--- movie-frontend/src/components/Trailer.tsx
+++ movie-frontend/src/components/Trailer.tsx
@@ -1,8 +1,19 @@
+import {useParams} from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import './Trailer.css';
 
-export const Trailer = () => {
+
+const Trailer = () => {
+
+    let params = useParams();
+    let key = params.ytTrailerId;
+
   return (
-    <div className="bg-gray-900 min-h-screen flex items-center justify-center">
-        Trailer
+    <div className="react-player-container">
+      {(key!=null)?<ReactPlayer controls="true" playing={true} url ={`https://www.youtube.com/watch?v=${key}`} 
+      width = '100%' height='100%' />:null}
     </div>
-  );
-};
+  )
+}
+
+export default Trailer
